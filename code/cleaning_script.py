5df4d2dae6377b638f152e03ec65f30f453f7a2d
AlexSpradling <alexspradling@gmail.com>
2022-11-29 23:49:25 -0500

    col unique function returns dict

--- code/cleaning_script.py
+++ code/cleaning_script.py
@@ -1,5 +1,6 @@
 import re
 import numpy as np
+import pandas as pd
 
 
 # the provided .txt file breaks the categories up into NOMINAL, ORDINAL, CONTINUOUS, and DISCRETE variables. I'll do that here
@@ -7,7 +8,7 @@ import numpy as np
 
 NOMINAL = ['ms_subclass', 'ms_zoning', 'street', 'alley', 'land_contour', 'lot_config', 'neighborhood', 'condition_1', 'condition_2', 'bldg_type',
 'house_style', 'roof_style', 'roof_matl', 'exterior_1st', 'exterior_2nd', 'mas_vnr_type', 'foundation', 'heating', 'central_air', 'garage_type', 'misc_feature',
-'sale_type', 'sale_type']
+'sale_type']
 
 ORDINAL = ['lot_shape', 'utilities', 'land_slope', 'house_style', 'overall_qual', 'overall_cond', 'exter_qual', 'exter_cond', 'bsmt_qual',
 'bsmt_cond', 'bsmt_exposure', 'bsmtfin_type_1', 'bsmtfin_type_2', 'heating_qc', 'electrical', 'kitchen_qual', 'functional', 'fireplace_qu', 'garage_finish',
@@ -19,6 +20,15 @@ DISCRETE = ['year_built', 'year_remod/add', 'bsmt_full_bath', 'bsmt_half_bath', 
 CONTINUOUS = ['lot_frontage', 'lot_area', 'mas_vnr_area', 'bsmtfin_sf_1', 'bsmtfin_sf_2','bsmt_unf_sf',
 'total_bsmt_sf', '1st_flr_sf', '2nd_flr_sf', 'gr_liv_area', 'garage_area', 'wood_deck_sf', 'open_porch_sf',
 'enclosed_porch', '3ssn_porch', 'screen_porch', 'pool_area','misc_val']
+
+
+def unique_by_col(category, df):
+    items = {}
+    for col in df[category].columns:
+        items.update({col:df[category][col].unique()}) 
+    return items
+
+
 
 
 def is_outlier(points, thresh=3.5):
