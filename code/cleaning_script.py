2b744e6a09056d21994b0730d02b875e09f24d03
AlexSpradling <alexspradling@gmail.com>
2022-11-29 21:18:42 -0500

    Added categories to cleaning script

--- code/cleaning_script.py
+++ code/cleaning_script.py
@@ -1,19 +1,25 @@
 import re
 import numpy as np
 
-def snake_case(df):
-    """
-    param df: pandas Dataframe()
-    :return: df with columns snake cased
-    """
-    df.columns = ['_'.join(re.findall('[A-Z][a-z]*', x)) for x in df.columns]
-    return df
 
+# the provided .txt file breaks the categories up into NOMINAL, ORDINAL, CONTINUOUS, and DISCRETE variables. I'll do that here
+# so I can reference them thoughout multiple notebooks
 
-def lower_case_columns(df):
-    df.columns = [col.lower() for col in df.columns]
+NOMINAL = ['ms_subclass', 'ms_zoning', 'street', 'alley', 'land_contour', 'lot_config', 'neighborhood', 'condition_1', 'condition_2', 'bldg_type',
+'house_style', 'roof_style', 'roof_matl', 'exterior_1st', 'exterior_2nd', 'mas_vnr_type', 'foundation', 'heating', 'central_air', 'garage_type', 'misc_feature',
+'sale_type', 'sale_type']
 
-    return df
+ORDINAL = ['lot_shape', 'utilities', 'land_slope', 'house_style', 'overall_qual', 'overall_cond', 'exter_qual', 'exter_cond', 'bsmt_qual',
+'bsmt_cond', 'bsmt_exposure', 'bsmtfin_type_1', 'bsmtfin_type_2', 'heating_qc', 'electrical', 'kitchen_qual', 'functional', 'fireplace_qu', 'garage_finish',
+'garage_qual', 'garage_cond', 'paved_drive', 'pool_qc', 'fence']
+
+DISCRETE = ['year_built', 'year_remod/add', 'bsmt_full_bath', 'bsmt_half_bath', 'full_bath', 'half_bath', 'bedroom_abvgr','kitchen_abvgr',
+'totrms_abvgrd', 'fireplaces', 'garage_yr_blt', 'garage_cars', 'mo_sold', 'yr_sold']
+
+CONTINUOUS = ['lot_frontage', 'lot_area', 'mas_vnr_area', 'bsmtfin_sf_1', 'bsmtfin_sf_2','bsmt_unf_sf',
+'total_bsmt_sf', '1st_flr_sf', '2nd_flr_sf', 'gr_liv_area', 'garage_area', 'wood_deck_sf', 'open_porch_sf',
+'enclosed_porch', '3ssn_porch', 'screen_porch', 'pool_area','misc_val']
+
 
 def is_outlier(points, thresh=3.5):
     """
